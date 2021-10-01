3280a3ac069ae54b783dec09b98c3ab05da39220
BasmaElsisy1 <88114356+BasmaElsisy1@users.noreply.github.com>
2021-10-01 10:22:05 +0200

    Add files via upload

---
+++ webpack.dev.js
@@ -0,0 +1,52 @@
+const path = require ("path");
+const webpack = require ("webpack");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const {CleanWebpackPlugin} = require ("clean-webpack-plugin");
+const loader = require ('sass-loader');
+
+
+module.exports={
+    entry: './src/client/index.js',
+    mode : "development",
+    target: 'node',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
+    devtool: 'source-map',
+    stats: 'verbose',
+    module:{ 
+        rules: [
+            {
+                test: '/\.js$/',
+                exclude: /node.modules/,
+                loader: "babel-loader"
+            },
+
+            {
+                test: /\.scss$/,
+                use:[ 'style-loader' , 'css-loader' , 'sass-loader']
+            }
+        ]
+
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html"
+        }),
+
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        })
+    
+    ]
+
+}
